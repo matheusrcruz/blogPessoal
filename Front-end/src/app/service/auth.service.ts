e3ebb07bff0b2d54c66329d8327c5c992ed83c88
matheusrcruz <git.dev@outlook.com>
2021-05-03 16:29:29 -0300

    Fix: Error para renderizar componente apos inserir ngIf

--- Front-end/src/app/service/auth.service.ts
+++ Front-end/src/app/service/auth.service.ts
@@ -3,25 +3,28 @@ import { HttpClient} from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { User } from '../models/User';
 import { UserLogin } from '../models/UserLogin';
+import { environment } from 'src/environments/environment.prod';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-
   //Inicie o modulo HttpClient na assinatura do constuctor
   constructor(private http: HttpClient) { }
-  
   // Observable Olhe userLogin
           //[Sintaxe Metodo, alias: objeto ] 
-  entrar(userLogin: UserLogin): Observable<UserLogin> {
+  entrar(userLogin: UserLogin): Observable<UserLogin>{//refactor template string
     return this.http.post<UserLogin>('http://localhost:8080/usuarios/logar', userLogin)
   }
-
-  cadastrar(user: User): Observable<User> {
+  cadastrar(user: User): Observable<User>{
     return this.http.post<User>('http://localhost:8080/usuarios/cadastrar', user)
-
-
+  }
+  logar(){
+  let ok : boolean = false
+  if(environment.token != ''){
+    ok = true;
+  }
+  return ok;
   }
 
 }
