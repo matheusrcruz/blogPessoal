d6b5345bc65d8c2d572afe24343ed7b37d5060c8
matheusrcruz <git.dev@outlook.com>
2021-05-05 11:06:31 -0300

    ADD: Corrigindo error no botão sair

--- Front-end/src/app/app.module.ts
+++ Front-end/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,7 +9,6 @@ import { RodapeComponent } from './rodape/rodape.component';
 import { MenuComponent } from './menu/menu.component';
 import { EntrarComponent } from './entrar/entrar.component';
 import { CadastrarComponent } from './cadastrar/cadastrar.component';
-import { FormsModule } from '@angular/forms';
 import { InicioComponent } from './inicio/inicio.component';
 
 @NgModule({
