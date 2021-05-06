802c6984a2422383d30b3df617ed9b4c61e5baf0
matheusrcruz <git.dev@outlook.com>
2021-05-06 16:02:21 -0300

    FIX: Bug ao salvar temas em lista de temas cosumo da api

---
+++ Front-end/src/app/service/tema.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
+import { Tema } from '../models/Tema';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TemaService {
+
+  constructor(private http: HttpClient) { }
+
+  token = {
+    headers: new HttpHeaders().set('Authorization', environment.token)//Passar o token no cabeçalho da req
+  }
+  getAllTema(): Observable<Tema[]> {
+    return this.http.get<Tema[]>('http://localhost:8080/tema', this.token);//pega todos os temas e validar o token passado
+  }
+  postTema(tema: Tema): Observable<Tema> {
+    return this.http.post<Tema>('http://localhost:8080/tema', tema, this.token);
+  }
+}
